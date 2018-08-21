4302013ae053b7fc880f3308a7aa0d8ac50be72d
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-21 17:28:37 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,17 @@ def array_with_two_elements
   array = [1, 2]
 end
 
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  arr[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.
